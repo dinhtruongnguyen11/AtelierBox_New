a7d9345561be6a99fcdae4c8dd83ed38adc70911
JosephAllan1998 <vumanhtuan1010@gmail.com>
2021-01-20 22:38:56 +0700

    Update Detail Window + Add Product Image
    Thêm binding cho detail window và thêm hình ảnh sản phẩm

--- AppMain/DetailWindow.xaml.cs
+++ AppMain/DetailWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Threading;
 
 namespace AppMain
 {
@@ -11,16 +12,58 @@ namespace AppMain
     /// </summary>
     public partial class DetailWindow : Window
     {
-        public Product p;
+        public static Product p;
+        public static CartProduct cartProduct;
+        public DispatcherTimer timer;
         public DetailWindow()
         {
             InitializeComponent();
-            p = new Product { Price = 19.15, Instock = 10 };
+
+            timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 0, 1);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+
+            p = new Product
+            {
+                Active = "Active",
+                CatCode = 1,
+                DateCreate = DateTime.Now,
+                Description = "Noni juice is a globally popular health beverage originating in the tropics. Traditional Tahitian healers believe the noni plant to be useful for a wide range of maladies, and noni juice consumers throughout the world have similar perceptions. Nevertheless, human clinical trials are necessary for a precise understanding of what the health benefits of noni juice are. A review of published human intervention studies suggests that noni juice may provide protection against tobacco smoke-induced DNA damage, blood lipid and homocysteine elevation as well as systemic inflammation. Human intervention studies also indicate that noni juice may improve joint health, increase physical endurance, increase immune activity, inhibit glycation of proteins, aid weight management, help maintain bone health in women, help maintain normal blood pressure, and improve gum health. Further, these studies point to notable antioxidant activity in noni juice, more so than other fruit juices which served as trial placebos. It is this antioxidant effect and its interaction with the immune system and inflammation pathways that may account for many of the observed health benefits of noni juice. However, the existing evidence does have some limitations as far as its general application to noni juice products; all the peer-reviewed human interventions studies to date have involved only one source of French Polynesian noni juice. Geographical factors and variations in processing methods are known to produce commercial noni juice products with divergent phytochemical and nutrient compositions. Therefore, other sources of noni products may have different toxicological and pharmacological profiles.",
+                ID = 1,
+                ImgSrc = "bottle.png",
+                Instock = 10,
+                Name = "Tree of health noni juice".ToUpper(),
+                Price = 19.15,
+                UserCreate = "Admin"
+            };
+
+            cartProduct = new CartProduct
+            {
+                ID = 1,
+                Name = "Tree of health noni juice".ToUpper(),
+                Price = 19.15,
+                Quantity = 0
+            }; 
+
+            gridDetail.DataContext = p;
         }
-        public DetailWindow(Product _product)
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            var now = DateTime.Now;
+            string dateFormat = now.ToString("dd-MM-yyyy");
+            tbDate.Text = $"{now.DayOfWeek}\n{dateFormat}";
+            //tbTime.Text = $"{now.Hour}:{now.Minute}";
+            tbTime.Text = $"{now.ToString("HH:mm")}";
+        }
+
+        public DetailWindow(Product _product, ref CartProduct _cart)
         {
             InitializeComponent();
             p = _product;
+            cartProduct = _cart;
+            gridDetail.DataContext = p;
         }
 
         private void btnPlus_Click(object sender, RoutedEventArgs e)
@@ -56,7 +99,10 @@ namespace AppMain
         private void btnAdd_Click(object sender, RoutedEventArgs e)
         {
             //New cart
-            //Add cart
+            cartProduct.Quantity = int.Parse(txtQuantity.Text);
+            //Send back cart
+            //this.DialogResult = true;
+            this.Close();
             //Cancel
         }
 
